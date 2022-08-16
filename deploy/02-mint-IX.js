b05275906c689bf7deb174eede5267e6f7f9e274
Jason Wang <wanghung07@gmail.com>
2022-08-16 13:08:54 +0800

    add set token uri unit test

---
+++ deploy/02-mint-IX.js
@@ -0,0 +1,23 @@
+const { deployments, getNamedAccounts, network } = require("hardhat")
+const { verify } = require("../utils/verify")
+const { devChain } = require("../helper-hardhat-config")
+
+module.exports = async () => {
+    const { deploy, log } = deployments
+
+    const { deployer } = await getNamedAccounts()
+
+    const chainId = network.config.chainId
+
+    const ix = await deploy("IX", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitConfirmations: network.config.waitConfirmations || 1,
+    })
+    if (!devChain.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        await verify(ix.address, [])
+    }
+}
+
+module.exports.tags = ["all", "ix"]
--- test/unit/IX.test.js
+++ test/unit/IX.test.js
@@ -1,32 +1,43 @@
 const { deployments, ethers, getNamedAccounts } = require("hardhat")
-const { assert, expect } = require("chai")
 const { devChain } = require("../../helper-hardhat-config")
+const { expect, assert } = require("chai")
 
-describe("IX Testing", async () => {
-    let ix
-    let deployer
+!devChain.includes(network.name)
+    ? describe.skip
+    : describe("IX Testing", async () => {
+          let ix
+          let deployer
 
-    const TOKEN_ID = 1
-    const AMOUNT = 10
+          const TOKEN_ID = 1
+          const AMOUNT = 10
 
-    beforeEach(async () => {
-        // const {deployer} = await getNamedAccounts()
-        deployer = (await getNamedAccounts()).deployer
+          beforeEach(async () => {
+              // const {deployer} = await getNamedAccounts()
+              deployer = (await getNamedAccounts()).deployer
 
-        // run our .js in delpoy folder
-        await deployments.fixture(["all"])
+              // run our .js in delpoy folder
+              await deployments.fixture(["all"])
 
-        ix = await ethers.getContract("IX", deployer)
-    })
+              ix = await ethers.getContract("IX", deployer)
+          })
 
-    describe("mint function", async () => {
-        it("You're not the owner", async () => {
-            const accounts = await ethers.getSigners()
-            const accountZero = accounts[0]
-            const ixNotOwner = await ix.connect(accountZero)
+          describe("mint function", async () => {
+              it("You're not the owner", async () => {
+                  const accounts = await ethers.getSigners()
+                  const accountZero = accounts[0]
+                  const ixNotOwner = await ix.connect(accountZero)
 
-            await expect(ixNotOwner.mint(accountZero.address, TOKEN_ID, AMOUNT))
-                .to.be.reverted
-        })
-    })
-})
+                  await expect(
+                      ixNotOwner.mint(accountZero.address, TOKEN_ID, AMOUNT)
+                  ).to.be.reverted
+              })
+          })
+
+          describe("setTokenURI", async () => {
+              it("Set token uri", async () => {
+                  await ix.setTokenURI(0, "https://")
+                  const tokenId = await ix.uri(0)
+                  assert.equal(tokenId, "https://")
+              })
+          })
+      })
